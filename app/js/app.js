b3b7f3b4cad697259a1a8582fad3d704bb57901f
Hay Kranen <huskyr@gmail.com>
2018-02-17 01:15:56 +0100

    Old view new becomes app, removing old app

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,14 @@
-import View from './view.js';
-window.__view__ = new View();
+import Vue from 'vue';
+import router from './router.js';
+import CmFooter from './components/cm-footer.vue';
+import './filters.js';
+
+window.__app__ = new Vue({
+    el : "main",
+
+    router,
+
+    components : {
+        CmFooter
+    }
+});
